ad0e69b4b9d90f57e30e9e65da04ad006dbce8aa
Padmanabhan Ayyaswamy <sai.padmanabhan@gmail.com>
2025-02-10 12:31:27 +0530

    Updating comments

--- agf014ea-dev-devkit/niosv_g/ci_basic_operations/sources/sw/app/custom_instr_app.c
+++ agf014ea-dev-devkit/niosv_g/ci_basic_operations/sources/sw/app/custom_instr_app.c
@@ -1,117 +1,115 @@
-#include <stdio.h> // Include standard input/output library for printf
-#include <inttypes.h> // Include integer types for fixed-width integers
-#include "system.h" // Include system-specific definitions
-#include "altera_avalon_sysid_qsys_regs.h" // Include definitions for the Altera Avalon SYSID registers
+#include <stdio.h>
+#include <inttypes.h>
+#include "system.h"
+#include "altera_avalon_sysid_qsys_regs.h"
 
-// Function to perform custom instruction operations for processing element 1 (PE1)
+// Function to perform various arithmetic and bitwise operations using PE1
 void custom_inst_pe_1() {
-    uint32_t op; // Declare a variable to hold operation results
+    uint32_t op;
     printf("************************************************************************* \n");
-    printf("PE1 operations \n"); // Print header for PE1 operations
+    printf("PE1 operations \n");
     printf("************************************************************************* \n");
     
-    // Perform 1's complement operation on DATA1:0xff
+    // 1's complement operation
     op = basic_operations(0xff,0,0,0);    
     printf("1's complement of DATA1:0xff \nExpected Output:ffffff00 \nActual Output:%lx \n", op);    
 
     printf("************************************************************************* \n");   
-    // Perform 2's complement operation on DATA1:0xff
+    // 2's complement operation
     op =basic_operations(0xff,0,1,0);
     printf("2's complement of DATA1:0xff \nExpected Output:ffffff01 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");    
-    // Perform multiplication of DATA1:0x1f and DATA2:0x80
+    // Multiplication operation
     op = basic_operations(0x1f,0x80,2,0);
     printf("Multiplication of DATA1:0x1f and DATA2:0x80 \nExpected Output:f80 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Perform bit reversal on DATA1:0x1f
+    // Bit reversal operation
     op =basic_operations(0x1f,0x00,3,0);
     printf("Bit Reversal of DATA1:0x1f \nExpected Output:f8000000 \nActual Output:%lx \n", op);
     
     printf("************************************************************************* \n");
-    // Perform byte reversal on DATA1:0x1f
+    // Byte reversal operation
     op =basic_operations(0x1f, 0x00,4,0);
     printf("Byte Reversal of DATA1:0x1f \nExpected Output:1f000000 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Perform word reversal on DATA1:0x1f
+    // Word reversal operation
     op =basic_operations(0x1f, 0x00,5,0);
     printf("Word Reversal of DATA1:0x1f \nExpected Output:1f0000 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Merge lower dword of DATA1:0x74009078 and DATA2:0x82007083
+    // Merging lower dword operation
     op =basic_operations(0x74009078,0x82007083,6,0);
     printf("Merge Lower-dword DATA1:0x74009078 and DATA2:0x82007083 \nExpected Output:90787083 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Merge higher dword of DATA1:0x74009078 and DATA2:0x82007083
+    // Merging higher dword operation
     op =basic_operations(0x74009078,0x82007083,7,0);
     printf("Merge Higher-dword of DATA1:0x74009078 and DATA2:0x82007083 \nExpected Output:74008200 \nActual Output:%lx \n", op);
-    
-    printf("End of PE1 operations \n"); // Indicate the end of PE1 operations
+    printf("End of PE1 operations \n");
     printf("************************************************************************* \n");
 }
 
-// Function to perform custom instruction operations for processing element 2 (PE2)
+// Function to perform similar operations using PE2
 void custom_inst_pe_2() {
-    uint32_t op; // Declare a variable to hold operation results
+    uint32_t op;
     printf("************************************************************************* \n");
-    printf("PE2 operations \n"); // Print header for PE2 operations
+    printf("PE2 operations \n");
     printf("************************************************************************* \n");
     
-    // Perform 1's complement operation on DATA1:0xff
+    // 1's complement operation
     op = comp_1s(0xff,0,0);    
     printf("1's complement of DATA1:0xff\nExpected Output:ffffff00 \nActual Output:%lx \n", op);    
 
     printf("************************************************************************* \n");   
-    // Perform 2's complement operation on DATA1:0xff
+    // 2's complement operation
     op =comp_2s(0xff,0,0);
     printf("2's complement of DATA1:0xff\nExpected Output:ffffff01 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");    
-    // Perform multiplication of DATA1:0x1f and DATA2:0x80
+    // Multiplication operation
     op = multiply(0x1f,0x80,0);
     printf("Multiplication of DATA1:0x1f and DATA2:0x80 \nExpected Output:f80 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Perform bit reversal on DATA1:0x111fff
+    // Bit reversal operation
     op =bit_reversal(0x111fff,0x00,0);
     printf("Bit Reversal of DATA1:0x111fff \nExpected Output:fff88800 \nActual Output:%lx \n", op);
     
     printf("************************************************************************* \n");
-    // Perform byte reversal on DATA1:0x111fff
+    // Byte reversal operation
     op =byte_reversal(0x111fff, 0x00,0);
     printf("Byte Reversal of DATA1:0x111fff \nExpected Output:ff1f1100 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Perform word reversal on DATA1:0x111fff
+    // Word reversal operation
     op =word_reversal(0x111fff, 0x00,0);
     printf("Word Reversal of DATA1:0x111fff \nExpected Output:1fff0011 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Merge lower dword of DATA1:0x74009078 and DATA2:0x82007083
+    // Merging lower dword operation
     op =merge_lower(0x74009078,0x82007083,0);
     printf("Merge Lower-dword of DATA1:0x74009078 and DATA2:0x82007083 \nExpected Output:90787083 \nActual Output:%lx \n", op);
 
     printf("************************************************************************* \n");
-    // Merge higher dword of DATA1:0x74009078 and DATA2:0x82007083
+    // Merging higher dword operation
     op =merge_higher(0x74009078,0x82007083,0);
     printf("Merge Higher-dword of DATA1:0x74009078 and DATA2:0x82007083 \nExpected Output:74008200 \nActual Output:%lx \n", op);
-    
-    printf("End of PE2 operations \n"); // Indicate the end of PE2 operations
+    printf("End of PE2 operations \n");
     printf("************************************************************************* \n");
 }
 
-// Main function to execute the program
+// Main function to execute tests and display system ID
 int main() {
-    int sys_id; // Declare a variable to hold the system ID
+    int sys_id;
     printf("Now running custom instruction basic test...\n");
     printf ("Hello world \n");
-    custom_inst_pe_1(); // Call function for PE1 operations
-    custom_inst_pe_2(); // Call function for PE2 operations
+    custom_inst_pe_1();
+    custom_inst_pe_2();
     printf ("Print the value of System ID \n");
-    sys_id = IORD_ALTERA_AVALON_SYSID_QSYS_ID(SYSID_QSYS_0_BASE); // Read system ID from the peripheral
+    sys_id = IORD_ALTERA_AVALON_SYSID_QSYS_ID(SYSID_QSYS_0_BASE);
     printf ("System ID from Peripheral core is 0x%X \n",sys_id);
     printf("Bye world!\n");
     fflush(stdout);
