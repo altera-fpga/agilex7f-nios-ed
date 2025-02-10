b12fa040385fb6ec9f94eb49863311da8b3b4426
Padmanabhan Ayyaswamy <sai.padmanabhan@gmail.com>
2025-02-10 12:32:22 +0530

    Updated comments to hello.c

--- agf014ea-dev-devkit/niosv_g/ecc_lite/sources/sw/app/hello.c
+++ agf014ea-dev-devkit/niosv_g/ecc_lite/sources/sw/app/hello.c
@@ -3,29 +3,33 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h> // Standard I/O functions
-#include <unistd.h> // Standard library for usleep function
-#include "io.h" // I/O functions for memory-mapped I/O
-#include "system.h" // System definitions
-#include "altera_avalon_sysid_qsys_regs.h" // Altera Avalon SysID Qsys register definitions
+#include <stdio.h>
+#include <unistd.h>
+#include "io.h"
+#include "system.h"
+#include "altera_avalon_sysid_qsys_regs.h"
 
-// Function to print "Hello world" messages in a loop
+// Function to print a message repeatedly
 void looper() {
-    for (int i = 0; i < 10000; ++i) { // Loop 10000 times
-        printf("Hello world, this is the Nios V/g cpu checking in %d...\n", i); // Print message with loop counter
+    for (int i = 0; i < 10000; ++i) {
+        printf("Hello world, this is the Nios V/g cpu checking in %d...\n", i);
     }
 }
 
 int main() {
-    int sys_id; // Variable to store system ID
+    int sys_id;
+    
+    // Run the looper function to print messages
+    looper();
+    
+    // Introduce a short delay before reading System ID
+    usleep(1000);
 
-    looper(); // Call the looper function
-    usleep(1000); // Sleep for 1000 microseconds
-
-    printf("Print the value of System ID \n"); // Print message before reading system ID
-    sys_id = IORD_ALTERA_AVALON_SYSID_QSYS_ID(SYSID_QSYS_0_BASE); // Read system ID
-    printf("System ID from Peripheral core is 0x%X \n", sys_id); // Print system ID
-
-    printf("Bye world!\n"); // Print goodbye message
-    return 0; // Exit the application
+    // Read and print the System ID from the peripheral core
+    printf ("Print the value of System ID \n");
+    sys_id = IORD_ALTERA_AVALON_SYSID_QSYS_ID(SYSID_QSYS_0_BASE); // Read System ID from the Qsys system ID peripheral
+    printf ("System ID from Peripheral core is 0x%X \n", sys_id);
+    
+    printf("Bye world!\n");
+    return 0;
 }
